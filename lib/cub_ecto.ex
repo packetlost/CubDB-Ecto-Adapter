e807095ec342c93fd15a0a291e14ea13780df07f
Raúl <wychouza@gmail.com>
2021-03-30 09:54:14 -0700

    initialized elixir project

---
+++ lib/cub_ecto.ex
@@ -0,0 +1,18 @@
+defmodule CubEcto do
+  @moduledoc """
+  Documentation for `CubEcto`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CubEcto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cub_ecto_test.exs
@@ -0,0 +1,8 @@
+defmodule CubEctoTest do
+  use ExUnit.Case
+  doctest CubEcto
+
+  test "greets the world" do
+    assert CubEcto.hello() == :world
+  end
+end

f09b5c4d1a1356faa283df837014c6e5ad8778e3
Raúl <wychouza@gmail.com>
2021-03-30 12:55:11 -0700

    Implemented Ecto's adapter behaviour

--- lib/cub_ecto.ex
+++ lib/cub_ecto.ex
@@ -1,18 +1,39 @@
 defmodule CubEcto do
-  @moduledoc """
-  Documentation for `CubEcto`.
-  """
 
-  @doc """
-  Hello world.
+  @behaviour Ecto.Adapter
 
-  ## Examples
+  @impl true
+  defmacro __before_compile__(_env), do: :ok
 
-      iex> CubEcto.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @impl true
+  @spec init(config :: Keyword.t()) :: {:ok, :supervisor.child_spec(), Ecto.Adapter.adapter_meta()}
+  def init(_config \\ []) do
+    child_spec = CubDB.child_spec(data_dir: "../cubecto", name: CubEcto, auto_compact: true)
+    {:ok, child_spec, %{}}
   end
+
+  @impl true
+  @spec checkout(Ecto.Adapter.adapter_meta(), Keyword.t(), (() -> any())) :: any()
+  def checkout(_adapter_meta, _config, function) do
+    function.()
+  end
+
+  @impl true
+  @spec dumpers(Ecto.Type.primitive(), Ecto.Type.t()) :: [(term() -> {:ok, term()} | :error) | Ecto.Type.t()]
+  def dumpers(:binary_id, type), do: [type, Ecto.UUID]
+  def dumpers(_primitive, type), do: [type]
+
+  @impl true
+  @spec loaders(Ecto.Type.primitive(), Ecto.Type.t()) :: [(term() -> {:ok, term()} | :error) | Ecto.Type.t()]
+  def loaders(:binary_id, type), do: [Ecto.UUID, type]
+  def loaders(_primitive, type), do: [type]
+
+  @impl true
+  @spec ensure_all_started(Keyword.t(), :permanent | :transient | :temporary) :: {:ok, [atom()]} | {:error, atom()}
+  def ensure_all_started(config, _type) do
+    with {:ok, _} = Application.ensure_all_started(:cub_ecto) do
+      {:ok, [config]}
+  end
+  end
+
 end
--- test/cub_ecto_test.exs
+++ test/cub_ecto_test.exs
@@ -2,7 +2,4 @@ defmodule CubEctoTest do
   use ExUnit.Case
   doctest CubEcto
 
-  test "greets the world" do
-    assert CubEcto.hello() == :world
-  end
 end
